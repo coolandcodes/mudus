f244d2f4aa44183b5dd25ca1e2511b195e4cd046
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2015-11-03 12:17:04 +0100

    Create emitter.js

---
+++ demo/modules/emitter.js
@@ -0,0 +1,6 @@
+/* emitter module */
+                  console.log(typoef require);
+                  console.log(typeof module);
+                  console.log(module.id);
+                  
+                  
