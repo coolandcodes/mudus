bf0152265980d84eeb6775f46b10801a9b11b6b0
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2015-11-05 11:03:44 +0100

    Update index.js

--- demo/modules/index.js
+++ demo/modules/index.js
@@ -1,1 +1,24 @@
-/* index module */
+/* the index module */
+  
+                          console.log(module.id);   
+ 
+                         var emtr = require('emitter'),
+      
+                          $$ = jQuery,
+
+                         // the module definition
+                          Index = {
+                              watch:function(evt){
+                                  $$('#check-board').on('click', function(e){
+                                       emtr.emit(evt, {x:e.clientX, y:e.clientY});
+                                  });
+                              }
+                          };
+
+                          emtr.on('pos', function(data){
+                               alert("x: "+data.x+", y: "+data.y);
+                          });
+
+                          // yet another way to export modules
+
+                          return Index;
