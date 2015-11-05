3fddd4828888d6545a9ba5201fcbb302dfffd0f6
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2015-11-05 11:06:18 +0100

    Update defSpec.js

--- tests/specs/defSpec.js
+++ tests/specs/defSpec.js
@@ -1,15 +1,19 @@
 
-describe("definition test for public interface", function(){
+describe("definition/use case tests for public interface", function(){
     /* 
     beforeEach(function () {
           this.addMatchers({
             
           });
      });*/
-     it("should have the 'ready' method defined", function () {
-          expect(Mudus.ready).toBeDefined();
-     });
      it("should have the 'load' method not defined ", function(){
           expect(Mudus.load).toBeUndefined();
      });
+     it("should have the 'ready' method defined", function () {
+          expect(Mudus.ready).toBeDefined();
+     });
+     it("should have the 'ready' method throw an error", function () {
+          expect(function(){ Mudus.ready(); }).toThrowError("nothing to load");
+     });
+     
 });
