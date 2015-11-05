8d012a830195fac717331c7911ce590455c9e7c7
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2015-11-05 11:02:27 +0100

    Update emitter.js

--- demo/modules/emitter.js
+++ demo/modules/emitter.js
@@ -1,6 +1,32 @@
-/* emitter module */
-                  console.log(typoef require);
-                  console.log(typeof module);
-                  console.log(module.id);
-                  
+/* emitter module */                      
+                      console.log(typeof require);  
+
+                      console.log(typeof module);                    
+
+                      console.log(module.id);
+                            
+                      // a simple Pub/Sub interface which is akin to that of NodeJS
+                            
+                      var handlers = {},
+                           
+                      // the module definition itself
+                           
+                          pubsub =  {
+                                      
+                                     on:function(evt, callback){
+                                              if(!handlers[evt]){
+                                                   handlers[evt] = callback;
+                                              }
+                                      },
+                                      emit:function(evt){
+                                              if(handlers[evt){
+                                                   handlers[evt].apply(null, [].slice.call(arguments, 1));
+                                              }
+                                     }
+                            };
+                            
+                       // this is one way to export module definitions (using Mudus)
+                            
+                        module.exports = pubsub;
+
                   
